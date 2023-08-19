32d59d628c6f6f4a634c85aa710aae33208def1a
Alexander Alexeev <san73rus@gmail.com>
2023-08-19 17:52:05 +0400

    No idea

---
+++ src/components/Footer_Text.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const Footer_Text = styled.p`
+  margin-left: 234px;
+  color: #B7B7B7;
+  font-size: 14px;
+  font-feature-settings: 'clig' off, 'liga' off;
+  font-weight: 400;
+  justify-content: center;
+`
+
+export default Footer_Text
